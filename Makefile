0e9570aa95f32ed53ac7834b55819a735b723413
MDBrothers <brothers.michael.d@gmail.com>
2015-08-04 17:39:55 -0500

    switched to gl 43 core from immediate

--- Makefile
+++ Makefile
@@ -1,10 +1,10 @@
-CXX_FLAGS = -std=c++11 -g
+CXX_FLAGS = -std=c++11 -O4
 THIRD_PARTY = -I/usr/local/hdf5/include -I/usr/local/trilinos/include -I/usr/local/Trilinos/include -L/usr/local/hdf5/lib -L/usr/local/trilinos/lib -L/usr/local/Trilinos/lib -lflann -lmpi -lhdf5 -lepetra -lepetraext -lteuchoscore -lteuchoscomm -lteuchosparameterlist -lnox -lnoxepetra -lloca -llocaepetra -I/usr/local/boost/include
 
 compile_tests: 
 	data_test
 
-libplot3d.so: plot3d.cpp plot3d.hpp plot3dVertexShader.vert plot3dFragmentShader.frag
+libplot3d.so: plot3d.cpp plot3d.hpp vertexShader.glsl fragmentShader.glsl
 	mpicxx $(CXX_FLAGS) -shared -fPIC plot3d.cpp -o libplot3d.so -lGLU -lGL -DGL_GLEXT_PROTOTYPES
 
 libdata.so: data.cpp data.hpp
--- plot3d.cpp
+++ plot3d.cpp
@@ -9,23 +9,52 @@ Colormap                cmap;
 XSetWindowAttributes    swa;
 XWindowAttributes       wa;
 XEvent                  xev;
-bool terminate = false, on_gpu = false;
+bool terminate = false;
 
-float                   TimeCounter, LastFrameTimeCounter, DT, prevTime = 0.0, FPS;
-struct timeval          tv, tv0;
-int                     Frame = 1, FramesPerFPS;
+// Type declarations
 
-GLfloat                 rotation_matrix[16];
-float                   rot_z = 0.0, rot_y = 0.0;
+struct Vertex{
+	float coords[4];
+	float color[4];
+};
+
+struct Matrix4x4{
+	float entries[16];
+};
+
+// Static globals
+
+static const Matrix4x4 I_MAT_4x4={{1.f,0.f,0.f,0.f,  0.f,1.f,0.f,0.f,  0.f,0.f,1.f,0.f,  0.f,0.f,0.f,1.f}};	  
+				
+enum buffer {MODEL_VERTICES};
+enum object {MODEL};
+
+static Matrix4x4 rotMat = I_MAT_4x4;
+static Matrix4x4 transMat = I_MAT_4x4;
+static Matrix4x4 projMat = I_MAT_4x4;
+float  rot[3]={0.f,0.f,0.f};
+float  trans[3]={0.f,0.f,0.f};
+float  cam[3]={0.f,0.f,0.f};
+float  eye[3]={0.f,0.f,0.f};
+float  up[3]={0.f,1.f,0.f};
+
+static unsigned int
+   programId,
+   vertexShaderId,
+   fragmentShaderId,
+   modelViewMatLoc,
+   projMatLoc,
+   buffer[1],
+   vao[1];
 
 /* A simple function that will read a file into an allocated char pointer buffer */
-char* filetobuf(char *file)
+char* filetobuf(const std::string file)
 {
     FILE *fptr;
     long length;
     char *buf;
  
-    fptr = fopen(file, "rb"); /* Open file for reading */
+    fptr = fopen(file.c_str(), "rb"); /* Open file for reading */
     if (!fptr) /* Return NULL on failure */
         return NULL;
     fseek(fptr, 0, SEEK_END); /* Seek to the end of the file */
@@ -39,15 +68,6 @@ char* filetobuf(char *file)
     return buf; /* Return the buffer */
 }
 
-void RotateModel() {
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-    glRotatef(.01, 0.0, 1.0, 0.0);
-    glRotatef(.01, 0.0, 0.0, 1.0);
-    glMultMatrixf(rotation_matrix);
-    glGetFloatv(GL_MODELVIEW_MATRIX, rotation_matrix);
-}
-
 void ExposeFunc(const int numVertices) {
     float  aspect_ratio;
     char   info_string[256];
@@ -57,45 +77,19 @@ void ExposeFunc(const int numVertices) {
     glViewport(0, 0, wa.width, wa.height);
     aspect_ratio = (float)(wa.width) / (float)(wa.height);
 
-//Set up projection (orthographic vs. perspective) and model view (camera location and aim)
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-// glOrtho(-2.50*aspect_ratio, 2.50*aspect_ratio, -2.50, 2.50, -200.0, 200.0);
-    gluPerspective(50.0, aspect_ratio, 1.0, 400.0);
-
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-
-    float eyeX(float(0.0)), eyeY(float(0.0)), eyeZ(-300.0);
-    float aimX(eyeX), aimY(eyeY), aimZ(0.0);
-    float upX(0.f), upY(1.f), upZ(0.f);
-
-    gluLookAt(eyeX, eyeY, eyeZ, aimX, aimY, aimZ, upX, upY, upZ);
-    glMultMatrixf(rotation_matrix);
+//Update the view and transformation matrices
+    for(int i(0); i<3; ++i){
+    	rotMat.entries[5*i] = rot[i];
+    	//transMat.entries[5*i] = trans[i];
+    	//projMat.entries[5*i] = 1.f;
+    }
 
 //Draw the model
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glDrawArrays(GL_LINE_STRIP, 0, numVertices);
 
-//Display graphics system stats
-/*
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    glOrtho(0, (float)wa.width, 0, (float)wa.height, -1., 1.);
-
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-
-    glColor3f(1.0, 1.0, 1.0);
-
-    sprintf(info_string, "%4.1f seconds * %4.1f fps at %i x %i", TimeCounter, FPS, wa.width, wa.height);
-    glRasterPos2i(10, 10);
-    glCallLists(strlen(info_string), GL_UNSIGNED_BYTE, info_string);
-
+//Update graphics system stats
     sprintf(info_string, "<up,down,left,right> rotate model * <F1> stop rotation ");
-    glRasterPos2i(10, wa.height-32);
-    glCallLists(strlen(info_string), GL_UNSIGNED_BYTE, info_string);
-*/
 
 //Swap display buffer with draw buffer
     glXSwapBuffers(dpy, win);
@@ -103,7 +97,6 @@ void ExposeFunc(const int numVertices) {
 
 //Create a GL window
 void CreateWindow() {
-
     if((dpy = XOpenDisplay(NULL)) == NULL) {
         printf("\n\tcannot connect to x server\n\n");
         exit(0);
@@ -141,7 +134,7 @@ void SetupGL() {
 
     glXMakeCurrent(dpy, win, glc);
     glEnable(GL_DEPTH_TEST);
-    glClearColor(0.00, 0.00, 0.00, 1.00);
+    glClearColor(0.f, 0.f, 0.f, 1.f);
 
     //Locate a font for text display purposes
     for(int font_size = 14; font_size < 32; font_size += 2) {
@@ -153,33 +146,6 @@ void SetupGL() {
             break;
         }
     }
-
-//Initialize the rotation matrix
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-    glGetFloatv(GL_MODELVIEW_MATRIX, rotation_matrix);
-}
-
-//Time counter functions
-void InitTimeCounter() {
-    gettimeofday(&tv0, NULL);
-    FramesPerFPS = 30;
-}
-
-void UpdateTimeCounter() {
-    LastFrameTimeCounter = TimeCounter;
-    gettimeofday(&tv, NULL);
-    TimeCounter = (float)(tv.tv_sec-tv0.tv_sec) + 0.000001*((float)(tv.tv_usec-tv0.tv_usec));
-    DT = TimeCounter - LastFrameTimeCounter;
-}
-
-void CalculateFPS() {
-    Frame ++;
-
-    if((Frame%FramesPerFPS) == 0) {
-        FPS = ((float)(FramesPerFPS)) / (TimeCounter-prevTime);
-        prevTime = TimeCounter;
-    }
 }
 
 //Cleanup method
@@ -188,7 +154,7 @@ void ExitPlotWindow() {
     glXDestroyContext(dpy, glc);
     XDestroyWindow(dpy, win);
     XCloseDisplay(dpy);
-    //exit(0);
+    exit(0);
 }
 
 //User input polling method
@@ -198,22 +164,22 @@ void CheckKeyboard() {
         char    *key_string = XKeysymToString(XkbKeycodeToKeysym(dpy, xev.xkey.keycode, 0, 0));
 
         if(strncmp(key_string, "Left", 4) == 0) {
-            rot_z = -45.0;
+            rot[0] = -45.f;
         }
 
         else if(strncmp(key_string, "Right", 5) == 0) {
-            rot_z = 45.0;
+            rot[0] = 45.f;
         }
         else if(strncmp(key_string, "Up", 2) == 0) {
-            rot_y = -45.0;
+            rot[0] = -45.f;
         }
 
         else if(strncmp(key_string, "Down", 4) == 0) {
-            rot_y = 45.0;
+            rot[0] = 45.f;
         }
         else if(strncmp(key_string, "F1", 2) == 0) {
-            rot_y = 0.0;
-            rot_z = 0.0;
+            rot[0] = 0.f;
+            rot[0] = 0.f;
         }
 
         else if(strncmp(key_string, "Escape", 5) == 0) {
@@ -226,99 +192,65 @@ void CheckKeyboard() {
 void plotCurve(const std::valarray<double>& vertices){
 	CreateWindow();
 	SetupGL();
-	InitTimeCounter();
 
 	// Copy whatever we get into an array of floats
-	std::valarray<float> verticesFormatted(vertices.size());
+	std::valarray<Vertex> model(vertices.size());
 	for(int i = 0; i < vertices.size(); ++i){
-		verticesFormatted[i] = vertices[i];	
-	}		
-	const int numVertices = verticesFormatted.size()/3;
+		model[i].coords[0] = vertices[i*3];	
+		model[i].coords[1] = vertices[i*3+1];	
+		model[i].coords[2] = vertices[i*3+2];	
+		model[i].coords[3] = 1.f;	
+		model[i].color[0] = vertices[i*3];
+		model[i].color[1] = vertices[i*3+1];
+		model[i].color[2] = vertices[i*3+2];
+		model[i].color[3] = 1.f;
+	}
 
-	/*
- 	* Copy the vertex values into a buffer on the GPU for fast drawing
- 	*/
+	// Compile vertex shader
+	char* vertexShader = filetobuf("vertexShader.glsl");
+	vertexShaderId = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShaderId, 1, &vertexShader, NULL);
+	glCompileShader(vertexShaderId);
 
-	/* Create a variable to hold the VBO identifier */
-	GLuint curveVBO;
-	 
-	/* This is a handle to the shader program */
-	GLuint shaderProgram;
+	// Compile fragment shader
+	char* fragmentShader = filetobuf("fragmentShader.glsl");
+	fragmentShaderId = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShaderId, 1, &fragmentShader, NULL);
+	glCompileShader(fragmentShaderId);
 
-	/* These pointers will receive the contents of our shader source code files */
-	GLchar *vertexSource, *fragmentSource;
+	// Don't need the source code anymore after compilation
+	free(fragmentShader);
+	free(vertexShader);
 
-	/* These are handles used to reference the shaders */
-	GLuint vertexShader, fragmentShader;
+	// Put together the programmable part of the graphics pipline, the shaders
+	programId = glCreateProgram();
+	glAttachShader(programId, vertexShaderId);
+	glAttachShader(programId, fragmentShaderId);
+	glLinkProgram(programId);
+	glUseProgram(programId);
 
-	const unsigned int shaderAttribute = 0;
+	// Create the Vertex Aray Object and Vertex Buffer Object and associate the data with the vertex shader
+	glGenVertexArrays(1, vao);
+	glGenBuffers(1, buffer);
+	glBindVertexArray(vao[MODEL]);
+	glBindBuffer(GL_ARRAY_BUFFER, buffer[MODEL_VERTICES]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex)*model.size(), &model[0], GL_STATIC_DRAW);
 
-	/*---------------------- Initialise VBO - (Note: do only once, at start of program) ---------------------*/
-	/* Create a new VBO and use the variable "curveVBO" to store the VBO id */
-	glGenBuffers(1, &curveVBO);
+	glVertexAttribPointer(0,4,GL_FLOAT,GL_FALSE, sizeof(Vertex), 0);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1,4,GL_FLOAT,GL_FALSE,sizeof(Vertex), (GLvoid*)sizeof(model[0].coords));
+	glEnableVertexAttribArray(1);
 
-	/* Make the new VBO active */
-	glBindBuffer(GL_ARRAY_BUFFER, curveVBO);
+	// Upload uniform matrices  
+	projMatLoc = glGetUniformLocation(programId,"projMat");
+	glUniformMatrix4fv(projMatLoc, 1, GL_TRUE, projMat.entries);
 
-	/* Upload vertex data to the video device */
-	glBufferData(GL_ARRAY_BUFFER, verticesFormatted.size()*sizeof(float), &verticesFormatted[0], GL_STATIC_DRAW);
-
-	/* Specify that our coordinate data is going into attribute index 0(shaderAttribute), and contains three floats per vertex */
-	glVertexAttribPointer(shaderAttribute, 3, GL_FLOAT, GL_FALSE, 0, 0);
-
-	/* Enable attribute index 0(shaderAttribute) as being used */
-	glEnableVertexAttribArray(shaderAttribute);
-
-	/* Make the new VBO active. */
-	glBindBuffer(GL_ARRAY_BUFFER, curveVBO);
-	/*-------------------------------------------------------------------------------------------------------*/
-
-	/*--------------------- Load Vertex and Fragment shaders from files and compile them --------------------*/
-	/* Read our shaders into the appropriate buffers */
-	vertexSource = filetobuf("plot3dVertexShader.vert");
-	fragmentSource = filetobuf("plot3dFragmentShader.frag");
-
-	/* Assign our handles a "name" to new shader objects */
-	vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-
-	/* Associate the source code buffers with each handle */
-	glShaderSource(vertexShader, 1, (const GLchar**)&vertexSource, 0);
-	glShaderSource(fragmentShader, 1, (const GLchar**)&fragmentSource, 0);
-
-	/* Free the temporary allocated memory */
-	free(vertexSource);
-	free(fragmentSource);
-
-	/* Compile our shader objects */
-	glCompileShader(vertexShader);
-	glCompileShader(fragmentShader);
-	/*-------------------------------------------------------------------------------------------------------*/
-
-	/*-------------------- Create shader program, attach shaders to it and then link it ---------------------*/
-	/* Assign our program handle a "name" */
-	shaderProgram = glCreateProgram();
-
-	/* Attach our shaders to our program */
-	glAttachShader(shaderProgram, vertexShader);
-	glAttachShader(shaderProgram, fragmentShader);
-
-	/* Bind attribute index 0 (shaderAttribute) to in_Position*/
-	/* "in_Position" will represent "verticesFormatted" array's contents in the vertex shader */
-	glBindAttribLocation(shaderProgram, shaderAttribute, "in_Position");
-
-	/* Link shader program*/
-	glLinkProgram(shaderProgram);
-	/*-------------------------------------------------------------------------------------------------------*/
-
-	/* Set shader program as being actively used */
-	glUseProgram(shaderProgram);
+	Matrix4x4 modelViewMat = I_MAT_4x4;	
+	modelViewMatLoc = glGetUniformLocation(programId, "modelViewMat");
+	glUniformMatrix4fv(modelViewMatLoc, 1, GL_TRUE, modelViewMat.entries);
 
 	while(not terminate){
-		UpdateTimeCounter();
-		CalculateFPS();
-		RotateModel();
-		ExposeFunc(numVertices);
+		ExposeFunc(model.size());
 		usleep(1000);
 		CheckKeyboard();
 	}
